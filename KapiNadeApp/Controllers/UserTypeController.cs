3a751d8a9b996e31f916d852525bd0f7d8a6274c
meanela <minela.prosic.mp@gmail.com>
2024-03-02 02:44:06 +0100

    CRUD za User Types

---
+++ KapiNadeApp/Controllers/UserTypeController.cs
@@ -0,0 +1,108 @@
+﻿using DatabaseLayer;
+using KapiNadeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KapiNadeApp.Controllers
+{
+    public class UserTypeController : Controller
+    {
+        KapiNadeDBEntities DB = new KapiNadeDBEntities();
+        public ActionResult AllUserTypes()
+        {
+            var usertypes = DB.UserTypeTables.ToList();
+            var listusertypes = new List<UserTypeMV>();
+            foreach (var usertype in usertypes)
+            {
+                var addusertype = new UserTypeMV();
+                addusertype.UserTypeID = usertype.UserTypeID;
+                addusertype.UserType = usertype.UserType;
+                listusertypes.Add(addusertype);
+            }
+            return View(listusertypes);
+        }
+        public ActionResult Create()
+        {
+            var usertype = new UserTypeMV();
+            return View(usertype);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserTypeMV userTypeMV)
+        {
+            if (ModelState.IsValid)
+            {
+                var userTypeTable = new UserTypeTable();
+                userTypeTable.UserTypeID = userTypeMV.UserTypeID;
+                userTypeTable.UserType = userTypeMV.UserType;
+                DB.UserTypeTables.Add(userTypeTable);
+                DB.SaveChanges();
+
+                return RedirectToAction("AllUserTypes");
+            }
+            return View(userTypeMV);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var usertype = DB.UserTypeTables.Find(id);
+            if (usertype == null)
+            {
+                return HttpNotFound();
+            }
+            var usertypemv = new UserTypeMV();
+            usertypemv.UserTypeID = usertype.UserTypeID;
+            usertypemv.UserType = usertype.UserType;
+            return View(usertypemv);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserTypeMV userTypeMV)
+        {
+            if (ModelState.IsValid)
+            {
+                var userTypeTable = new UserTypeTable();
+                userTypeTable.UserTypeID = userTypeMV.UserTypeID;
+                userTypeTable.UserType = userTypeMV.UserType;
+
+                DB.Entry(userTypeTable).State = EntityState.Modified;
+                DB.SaveChanges();
+                return RedirectToAction("AllUserTypes");
+            }
+            return View(userTypeMV);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var usertype = DB.UserTypeTables.Find(id);
+            if (usertype == null)
+            {
+                return HttpNotFound();
+            }
+            var usertypemv = new UserTypeMV();
+            usertypemv.UserTypeID = usertype.UserTypeID;
+            usertypemv.UserType = usertype.UserType;
+            return View(usertypemv);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int? id)
+        {
+            var usertype = DB.UserTypeTables.Find(id);
+            DB.UserTypeTables.Remove(usertype);
+            DB.SaveChanges();
+            return RedirectToAction("AllUserTypes");
+        }
+    }
+}
---
+++ KapiNadeApp/Models/UserTypeMV.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KapiNadeApp.Models
+{
+    public class UserTypeMV
+    {
+        public int UserTypeID { get; set; }
+        [Required(ErrorMessage ="Obavezno ispuniti*")]
+        [Display(Name = "User Type")]
+        public string UserType { get; set; }
+    }
+}
