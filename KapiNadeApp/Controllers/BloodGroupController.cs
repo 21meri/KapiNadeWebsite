d75a3d873d01b6955a63c9636e277d9f5c55ce3c
Merjem <116643674+21meri@users.noreply.github.com>
2024-03-02 04:27:14 +0100

    CRUD za Blood Groups

---
+++ KapiNadeApp/Controllers/BloodGroupController.cs
@@ -0,0 +1,131 @@
+﻿using DatabaseLayer;
+using KapiNadeApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KapiNadeApp.Controllers
+{
+    public class BloodGroupController : Controller
+    {
+        KapiNadeDBEntities DB = new KapiNadeDBEntities();
+        public ActionResult AllBloodGroups()
+        {
+            var bloodgroups = DB.BloodGroupsTables.ToList();
+            var listbloodgroups = new List<BloodGroupsMV>();
+            foreach (var bloodgroup in bloodgroups)
+            {
+                var addbloodgroup = new BloodGroupsMV();
+                addbloodgroup.BloodGroupID = bloodgroup.BloodGroupID;
+                addbloodgroup.BloodGroup = bloodgroup.BloodGroup;
+                listbloodgroups.Add(addbloodgroup);
+            }
+            return View(listbloodgroups);
+        }
+
+        public ActionResult Create()
+        {
+            var bloodgroup = new BloodGroupsMV();
+            return View(bloodgroup);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(BloodGroupsMV bloodGroupsMV)
+        {
+            if (ModelState.IsValid)
+            {
+                var checkbloodgroup = DB.BloodGroupsTables.Where(b => b.BloodGroup == bloodGroupsMV.BloodGroup).FirstOrDefault();
+                if (checkbloodgroup == null)
+                {
+                    var bloodGroupsTable = new BloodGroupsTable();
+                    bloodGroupsTable.BloodGroupID = bloodGroupsMV.BloodGroupID;
+                    bloodGroupsTable.BloodGroup = bloodGroupsMV.BloodGroup;
+                    DB.BloodGroupsTables.Add(bloodGroupsTable);
+                    DB.SaveChanges();
+
+                    return RedirectToAction("AllBloodGroups");
+                }
+                else
+                {
+                    ModelState.AddModelError("BloodGroup", "Already Exist!");
+                }
+            }
+            return View(bloodGroupsMV);
+                
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            var bloodgroup = DB.BloodGroupsTables.Find(id);
+            if (bloodgroup == null)
+            {
+                return HttpNotFound();
+            }
+            var bloodGroupsMV = new BloodGroupsMV();
+            bloodGroupsMV.BloodGroupID = bloodgroup.BloodGroupID;
+            bloodGroupsMV.BloodGroup = bloodgroup.BloodGroup;
+            return View(bloodGroupsMV);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(BloodGroupsMV bloodGroupsMV)
+        {
+            if (ModelState.IsValid)
+            {
+                    var checkbloodgroup = DB.BloodGroupsTables.Where(b => b.BloodGroup == bloodGroupsMV.BloodGroup && b.BloodGroupID != bloodGroupsMV.BloodGroupID).FirstOrDefault();
+                    if (checkbloodgroup == null)
+                    {
+                        var bloodGroupsTable = new BloodGroupsTable();
+                        bloodGroupsTable.BloodGroupID = bloodGroupsMV.BloodGroupID;
+                        bloodGroupsTable.BloodGroup = bloodGroupsMV.BloodGroup;
+
+                        DB.Entry(bloodGroupsTable).State = EntityState.Modified;
+                        DB.SaveChanges();
+                        return RedirectToAction("AllBloodGroups");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("BloodGroup", "Already Exist!");
+                }
+                }
+            return View(bloodGroupsMV);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            var bloodgroup = DB.BloodGroupsTables.Find(id);
+            if (bloodgroup == null)
+            {
+                return HttpNotFound();
+            }
+            var bloodGroupsMV = new BloodGroupsMV();
+            bloodGroupsMV.BloodGroupID = bloodgroup.BloodGroupID;
+            bloodGroupsMV.BloodGroup = bloodgroup.BloodGroup;
+            return View(bloodGroupsMV);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirm(int? id)
+        {
+            var bloodgroup = DB.BloodGroupsTables.Find(id);
+            DB.BloodGroupsTables.Remove(bloodgroup);
+            DB.SaveChanges();
+            return RedirectToAction("AllBloodGroups");
+        }
+
+
+
+
+    }
+}
---
+++ KapiNadeApp/Models/BloodGroupsMV.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace KapiNadeApp.Models
+{
+    public class BloodGroupsMV
+    {
+
+
+        public int BloodGroupID { get; set; }
+        [Required(ErrorMessage = "Obavezno ispuniti*")]
+        [Display(Name = "Blood Group")]
+        public string BloodGroup { get; set; }
+
+
+    }
+}
