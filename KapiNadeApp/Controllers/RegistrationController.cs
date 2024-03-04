933ff9836e5d8fd670c0d7f59b459df3f3b03897
meanela <minela.prosic.mp@gmail.com>
2024-03-04 20:37:16 +0100

    ubačena registracija za bolnice

--- KapiNadeApp/Controllers/RegistrationController.cs
+++ KapiNadeApp/Controllers/RegistrationController.cs
@@ -1,4 +1,5 @@
-﻿using KapiNadeApp.Models;
+﻿using DatabaseLayer;
+using KapiNadeApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,13 @@ namespace KapiNadeApp.Controllers
 {
     public class RegistrationController : Controller
     {
+        KapiNadeDBEntities DB = new KapiNadeDBEntities();
+        static RegistrationMV registrationmv;
         [HttpPost]
         [ValidateAntiForgeryToken]
       public ActionResult SelectUser(RegistrationMV registrationMV)
         {
+            registrationmv = registrationMV;
             if (registrationMV.UserTypeID == 2)
             {
                 return RedirectToAction("DonorUser");
@@ -33,22 +37,47 @@ namespace KapiNadeApp.Controllers
             {
                 return RedirectToAction("MainHome", "Home");
             }
-            var registration = new RegistrationMV();
-            return View(registration);
         }
 
     
     public ActionResult HospitalUser()
         {
-            return View();
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View(registrationmv);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
 
-        public ActionResult HospitalUser(HospitalMV hospitalMV)
+        public ActionResult HospitalUser(RegistrationMV registrationMV)
         {
-            return View();
+            if(ModelState.IsValid)
+            {
+                var checktitle = DB.HospitalTables.Where(h => h.Name == registrationMV.Hospital.Name.Trim()).FirstOrDefault();
+                if (checktitle == null)
+                {
+                    var user = new UserTable();
+                    user.Username = registrationMV.User.Username;
+                    user.Password = registrationMV.User.Password;
+                    user.Email = registrationMV.User.Email;
+                    user.AccountStatusID = 1;
+                    user.UserTypeID = registrationMV.UserTypeID;
+                    DB.UserTables.Add(user);
+
+                    var hospital = new HospitalTable();
+                    hospital.Name = registrationMV.Hospital.Name;
+                    hospital.Address = registrationMV.Hospital.Address;
+                    hospital.ContactNumber = registrationMV.Hospital.ContactNumber;
+                    hospital.Email = registrationMV.Hospital.Email;
+                    hospital.CityID = registrationMV.CityID;
+                    hospital.UserID = user.UserID;
+                    DB.HospitalTables.Add(hospital);
+                    DB.SaveChanges();
+                    return RedirectToAction("MainHome", "Home");
+                }
+    }
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationMV.CityID);
+            return View(registrationMV);
         }
 
 
@@ -57,14 +86,17 @@ namespace KapiNadeApp.Controllers
 
         public ActionResult DonorUser()
         {
-            return View();
+            ViewBag.UserTypeID = new SelectList(DB.UserTypeTables.Where(ut => ut.UserTypeID > 1).ToList(), "UserTypeID", "UserType", registrationmv.UserTypeID);
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View(registrationmv);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
 
-        public ActionResult DonorUser(DonorMV donorMV)
+        public ActionResult DonorUser(RegistrationMV registrationMV)
         {
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
             return View();
         }
 
@@ -72,14 +104,16 @@ namespace KapiNadeApp.Controllers
 
         public ActionResult BloodBankUser()
         {
-            return View();
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View(registrationmv);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
 
-        public ActionResult BloodBankUser(BloodBankMV bloodBankMV)
+        public ActionResult BloodBankUser(RegistrationMV registrationMV)
         {
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
             return View();
         }
 
@@ -87,14 +121,16 @@ namespace KapiNadeApp.Controllers
 
         public ActionResult SeekerUser()
         {
-            return View();
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
+            return View(registrationmv);
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
 
-        public ActionResult SeekerUser(SeekerMV seekerMV)
+        public ActionResult SeekerUser(RegistrationMV registrationMV)
         {
+            ViewBag.CityID = new SelectList(DB.CityTables.ToList(), "CityID", "City", registrationmv.CityID);
             return View();
         }
 
