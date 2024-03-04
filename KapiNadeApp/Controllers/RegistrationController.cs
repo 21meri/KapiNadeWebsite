ab16650ed76bca0d6a936975308582d76cc7f67d
Merjem <116643674+21meri@users.noreply.github.com>
2024-03-04 18:29:14 +0100

    pravljenje ruta za korisnike prilikom registracije (vraca view zavisno od tipa korisnika)

--- KapiNadeApp/Controllers/RegistrationController.cs
+++ KapiNadeApp/Controllers/RegistrationController.cs
@@ -9,10 +9,95 @@ namespace KapiNadeApp.Controllers
 {
     public class RegistrationController : Controller
     {
-      public ActionResult UserRegistration()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+      public ActionResult SelectUser(RegistrationMV registrationMV)
         {
+            if (registrationMV.UserTypeID == 2)
+            {
+                return RedirectToAction("DonorUser");
+            }
+            else if (registrationMV.UserTypeID == 3)
+            {
+                return RedirectToAction("SeekerUser");
+            }
+            else if (registrationMV.UserTypeID == 4)
+            {
+                return RedirectToAction("HospitalUser");
+            }
+            else if (registrationMV.UserTypeID == 5)
+            {
+                return RedirectToAction("BloodBankUser");
+            }
+            else
+            {
+                return RedirectToAction("MainHome", "Home");
+            }
             var registration = new RegistrationMV();
             return View(registration);
         }
+
+    
+    public ActionResult HospitalUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult HospitalUser(HospitalMV hospitalMV)
+        {
+            return View();
+        }
+
+
+
+
+
+        public ActionResult DonorUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult DonorUser(DonorMV donorMV)
+        {
+            return View();
+        }
+
+
+
+        public ActionResult BloodBankUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult BloodBankUser(BloodBankMV bloodBankMV)
+        {
+            return View();
+        }
+
+
+
+        public ActionResult SeekerUser()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public ActionResult SeekerUser(SeekerMV seekerMV)
+        {
+            return View();
+        }
+
+
     }
 }
