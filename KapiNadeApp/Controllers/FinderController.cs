fcec9a4b124f02ae476f17f42457471452e5f3fc
meanela <minela.prosic.mp@gmail.com>
2024-03-24 01:12:34 +0100

    - u FinderController dodan CompleteRequest() za kompletiranje zahtjeva - u ShowAllRequests dodan button Complete pod uslovom da je status zahtjeva prihvaćen

--- KapiNadeApp/Controllers/FinderController.cs
+++ KapiNadeApp/Controllers/FinderController.cs
@@ -357,6 +357,24 @@ namespace KapiNadeApp.Controllers
             return View(list);
         }
 
-
+        public ActionResult CompleteRequest(int? id)
+        {
+            if (string.IsNullOrEmpty(Convert.ToString(Session["Username"])))
+            {
+                return RedirectToAction("Login", "Home");
+            }
+            var request = DB.RequestTables.Find(id);
+            if(request.AcceptedTypeID == 1) //Donor
+            {
+                var donor = DB.DonorTables.Find(request.AcceptedID);
+                donor.LastDonationDate = DateTime.Now;
+                DB.Entry(donor).State = System.Data.Entity.EntityState.Modified;
+                DB.SaveChanges();
+            }
+            request.RequestStatusID = 3;
+            DB.Entry(request).State = System.Data.Entity.EntityState.Modified;
+            DB.SaveChanges();
+            return RedirectToAction("ShowAllRequests");
+        }
     }
 }
