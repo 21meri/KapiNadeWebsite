cc9bc5d8deee4582f8af4c7e467566430fdd5b56
meanela <minela.prosic.mp@gmail.com>
2024-04-24 04:25:54 +0200

    ispis poruke dobrodošlice na dashboard sa imenom korisnika

--- KapiNadeApp/Controllers/DashboardController.cs
+++ KapiNadeApp/Controllers/DashboardController.cs
@@ -9,24 +9,46 @@ namespace KapiNadeApp.Controllers
     public class DashboardController : Controller
     {
         // GET: Dashboard
+
         public ActionResult Donor()
         {
+            // Retrieve the username from the session
+            string name = Session["Name"] as string;
+
+            // Pass the username to the viewbag
+            ViewBag.Name = name;
+
             return View();
         }
 
         public ActionResult Seeker()
         {
+            // Retrieve the username from the session
+            string name = Session["Name"] as string;
+
+            // Pass the username to the viewbag
+            ViewBag.Name = name;
             return View();
         }
 
         public ActionResult Hospital()
         {
+            // Retrieve the username from the session
+            string name = Session["Name"] as string;
+
+            // Pass the username to the viewbag
+            ViewBag.Name = name;
             return View();
         }
 
 
         public ActionResult BloodBank()
         {
+            // Retrieve the username from the session
+            string name = Session["Name"] as string;
+
+            // Pass the username to the viewbag
+            ViewBag.Name = name;
             return View();
         }
 
